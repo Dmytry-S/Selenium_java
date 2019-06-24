59e953692448e7324c7ff78a59f37fbb1f41ff15
Dmytry-S <sviridoffd@gmail.com>
2019-06-24 22:24:12 +0300

    24.06

---
+++ src/DinDropd.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DinDropd {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dmytryi\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.spicejet.com/");
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='IXB']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//a[@value='CCJ'])[2]")).click();
+
+	}
+
+}
