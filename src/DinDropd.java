0bc6519fa9e51c2719a8cf4d0b8ee0d90917a7f9
Dmytry-S <sviridoffd@gmail.com>
2019-06-28 21:36:42 +0300

    28.06

--- src/DinDropd.java
+++ src/DinDropd.java
@@ -11,7 +11,8 @@ public class DinDropd {
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 		driver.findElement(By.xpath("//a[@value='IXB']")).click();
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("(//a[@value='CCJ'])[2]")).click();
+		// driver.findElement(By.xpath("(//a[@value='CCJ'])[2]")).click();
+		driver.findElement(By.xpath("//div[@id=\"glsctl00_mainContent_ddl_destinationStation1_CTNR\"]  //a[@value='CCJ']")).click();
 
 	}
 
